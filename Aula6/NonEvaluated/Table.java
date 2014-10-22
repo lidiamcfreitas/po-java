bbf70df71fccbe2fdf01179f8854c633fc15940e
Lidia <lidiamcfreitas@gmail.com>
2014-10-22 11:02:35 +0100

    added exercises of class 6

---
+++ Aula6/NonEvaluated/Table.java
@@ -0,0 +1,80 @@
+import java.util.Comparator;
+
+public class Table implements Comparable<Table>
+{
+    private int _size;
+    private int _vector[];
+    
+    public final static Comparator<Table> MAX_COMPARATOR = new maxComparator();
+    public final static Comparator<Table> LENGTH_COMPARATOR = new lengthComparator();
+    public Table(int size) { _size = size; _vector = new int[_size];}
+    
+    public int getValue(int position){
+        if (position < 0 && position > _size) throw new IllegalArgumentException("Must be between 0 and size");
+        return _vector[position]; 
+    }
+
+    public void setValue(int position, int value) throws IllegalArgumentException{
+        if (position < 0 && position > _size)
+            throw new IllegalArgumentException("Must be between 0 and size");
+        _vector[position] = value;
+    }
+
+    public void setAll(int value) {
+    	for(int i=0;i<_size;i++)
+            _vector[i] = value;
+    }
+
+    public int getSum(){
+        int sum = 0;
+
+        for(int i : _vector){
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int max(){
+        int j = _vector[0];
+        for (int i: _vector)
+            if ( i >= j) { i = j; j = i;}
+        return j;
+    }
+
+    public int getSize() { return _size; }
+
+    public int compareTo(Table table) throws NullPointerException{
+        if (table == null) throw new NullPointerException();
+        return getSum()- table.getSum();
+    }
+
+    private static class maxComparator implements Comparator<Table> {
+        public int compare(Table t1, Table t2) {
+            return t1.max() - t2.max();
+        }
+    }
+
+    private static class lengthComparator implements Comparator<Table> {
+        public int compare(Table t1, Table t2) {
+            return t1.getSize() - t2.getSize();
+        }
+    }
+    public static void main(String[] args)
+    {
+        Table t1 = new Table(10);
+        Table t2 = new Table(11);
+
+        t1.setAll(10);
+        t2.setAll(11);
+
+        System.out.println(t1.compareTo(t2));
+        System.out.println(t2.compareTo(t1));
+        System.out.println(t2.compareTo(t2));
+        System.out.println(Table.MAX_COMPARATOR.compare(t1, t2));
+        System.out.println(Table.MAX_COMPARATOR.compare(t2, t1));
+        System.out.println(Table.MAX_COMPARATOR.compare(t1, t1));
+        System.out.println(Table.LENGTH_COMPARATOR.compare(t1, t2));
+        System.out.println(Table.LENGTH_COMPARATOR.compare(t2, t1));
+        System.out.println(Table.LENGTH_COMPARATOR.compare(t1, t1));
+    }
+}
---
+++ Aula6/NonEvaluated/TableIterator.java
@@ -0,0 +1,45 @@
+import java.util.Iterator;
+
+public class TableIterator implements Iterable<Integer>
+{
+
+    private int _vector[];
+    private int _size;
+    public TableIterator(int size){
+    	_size = size;
+        _vector = new int[size];
+    }
+
+    public Iterator<Integer> iterator(){
+        class TableIter implements Iterator<Integer> {
+            int _index = 0;
+            @Override
+            public boolean hasNext() { return _index < TableIterator.this._size; }
+            @Override
+            public Integer next() { return TableIterator.this._vector[_index++]; }
+            @Override
+            public void remove() { throw new UnsupportedOperationException(); }
+
+        }
+
+        return new TableIter();
+    }
+
+    public void setValue(int pos, int value){
+    	_vector[pos] = value;
+    }
+
+    public static void main(String[] args)
+    {
+        TableIterator t1 = new TableIterator(5);
+
+        for(int i=0;i<5;i++)
+            t1.setValue(i,i);
+        
+        for(int i: t1)
+            System.out.println(i);
+        
+
+    }
+
+}
