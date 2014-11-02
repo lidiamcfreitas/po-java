c56b0e0376ba763149012c80aedfe85292abd357
lidiamcfreitas <lidiamcfreitas@gmail.com>
2014-11-02 20:17:55 +0000

    added file to save main cat characteristics

---
+++ Aula8/Cat.java
@@ -0,0 +1,20 @@
+public class Cat{
+    private int _age;
+    private double _weight;
+    private String _name;
+
+    public Cat(int age, double weight, String name)
+    {
+        _age = age;
+        _weight = weight;
+        _name = name;
+    }
+
+    public int getAge() { return _age; }
+
+    public double getWeight() { return _weight; }
+
+    public String getName() { return _name; }
+    
+
+}
---
+++ Aula8/ReadMainCat.java
@@ -0,0 +1,24 @@
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+public class ReadMainCat {
+
+    public static void main(String[] args) throws IOException {
+        
+        Cat cat = new Cat(9, 4.5, "Garfield");
+        
+        DataOutputStream _catData = new DataOutputStream(new BufferedOutputStream( new FileOutputStream("raw.dat")));
+        
+        _catData.writeInt(cat.getAge());
+        _catData.writeDouble(cat.getWeight());
+        _catData.writeUTF(cat.getName());
+        
+        _catData.close();
+        
+        
+    }
+    
+    
+}
